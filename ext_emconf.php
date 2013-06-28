eb7dd32d9b92c28b4320013934e0e5f08954e240
Andreas Schütte <a.schuette@bitmotion.de>
2013-06-28 16:02:22 +0200

    New Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '<a href="http://www.bitmotion.de" target="_blank">Bitmotion</a>',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'static_info_tables' => '',
