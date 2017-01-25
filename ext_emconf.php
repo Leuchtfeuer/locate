d98dbce4b7a551862a68f8ef72a1f39d9abe6e02
Daniel Koether <d.koether@bitmotion.de>
2017-01-25 19:00:58 +0100

    Fixed corrupt ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.2.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.6.99'
+            'typo3' => '6.2.0-7.6.99',
 			'static_info_tables' => '',
 			'php' => '5.3',
 		),
