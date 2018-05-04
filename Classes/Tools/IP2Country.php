a57d7f317b0a10e6f9e994fb2a8bbf52793d0547
Florian Wessels <f.wessels@bitmotion.de>
2018-05-04 13:10:05 +0200

    [TASK] Get rid of obsolete TYPO3_DB stuff

--- Classes/Tools/IP2Country.php
+++ Classes/Tools/IP2Country.php
@@ -18,33 +18,21 @@ abstract class IP2Country
     /**
      * Check the IP in the geoip table and returns iso 2 code for the current remote address
      *
-     * @param integer $IP as long remote address
-     * @return    false|string Example: DE
+     * @param     int           $ip     as long remote address
+     * @return    bool|string           Example: DE
      */
-    public static function GetCountryIso2FromIP($IP)
+    public static function GetCountryIso2FromIP(int $ip)
     {
-        if (class_exists(\TYPO3\CMS\Core\Database\ConnectionPool::class)) {
-            /** @var QueryBuilder $queryBuilder */
-            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_locate_ip2country');
-            $statement = $queryBuilder
-                ->select('iso2')
-                ->from('tx_locate_ip2country')
-                ->where($queryBuilder->expr()->lte('ipfrom', $queryBuilder->createNamedParameter($IP)))
-                ->andWhere($queryBuilder->expr()->gte('ipto', $queryBuilder->createNamedParameter($IP)))
-                ->execute();
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_locate_ip2country');
 
-            while ($row = $statement->fetch()) {
-                return $row['iso2'];
-            }
-        } else {
-            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('iso2', 'tx_locate_ip2country',
-                'ipfrom <= ' . $IP . ' AND ipto >= ' . $IP);
-            if ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
-                return $row['iso2'];
-            }
-        }
-
-        return false;
+        return $queryBuilder
+            ->select('iso2')
+            ->from('tx_locate_ip2country')
+            ->where($queryBuilder->expr()->lte('ipfrom', $queryBuilder->createNamedParameter($ip)))
+            ->andWhere($queryBuilder->expr()->gte('ipto', $queryBuilder->createNamedParameter($ip)))
+            ->execute()
+            ->fetchColumn(0);
     }
 
 }
