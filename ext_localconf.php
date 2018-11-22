ea259ccee7e8adad56c3ebf782a258138cd08053
Florian Wessels <f.wessels@bitmotion.de>
2018-11-22 14:43:24 +0100

    [TASK] Turn off logging by default

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,9 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3_MODE') || die;
 
+// Turn logging off by default
 $GLOBALS['TYPO3_CONF_VARS']['LOG']['Bitmotion']['Locate']['writerConfiguration'] = [
-    \TYPO3\CMS\Core\Log\LogLevel::INFO => [
-        TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-            'logFile' => 'typo3temp/var/logs/locate.log',
-        ],
-    ],
-
-    \TYPO3\CMS\Core\Log\LogLevel::ERROR => [
-        TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-            'logFile' => 'typo3temp/var/logs/locate.log',
-        ],
+    \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
+        \TYPO3\CMS\Core\Log\Writer\NullWriter::class => [],
     ],
 ];
