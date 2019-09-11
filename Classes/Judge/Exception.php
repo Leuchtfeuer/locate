24b3dedf3494fb9255acb1665c83077e675511f8
Florian Wessels <f.wessels@bitmotion.de>
2019-09-11 17:28:41 +0200

    [TASK] Mark exceptions as deprecated

--- Classes/Judge/Exception.php
+++ Classes/Judge/Exception.php
@@ -2,6 +2,20 @@
 declare(strict_types=1);
 namespace Bitmotion\Locate\Judge;
 
+use Throwable;
+
+/**
+ * @deprecated
+ */
 class Exception extends \Bitmotion\Locate\Exception
 {
+    /**
+     * @deprecated
+     */
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    {
+        trigger_error(sprintf('Using %s is deprecated. Use %s instead.', __CLASS__, \Bitmotion\Locate\Exception::class), E_USER_DEPRECATED);
+
+        parent::__construct($message, $code, $previous);
+    }
 }
