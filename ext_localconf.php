25e85a8912b47229fef042b738f95778f0e1b87f
Florian Wessels <f.wessels@bitmotion.de>
2019-12-09 16:34:50 +0100

    [TASK] Call user function in ext_localconf.php file

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,13 @@
 <?php
 defined('TYPO3_MODE') || die;
 
-// Turn logging off by default
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['Bitmotion']['Locate']['writerConfiguration'] = [
-    \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
-        \TYPO3\CMS\Core\Log\Writer\NullWriter::class => [],
-    ],
-];
+call_user_func(
+    function ($extensionKey) {
+        // Turn logging off by default
+        $GLOBALS['TYPO3_CONF_VARS']['LOG']['Bitmotion']['Locate']['writerConfiguration'] = [
+            \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
+                \TYPO3\CMS\Core\Log\Writer\NullWriter::class => [],
+            ],
+        ];
+    }, 'locate'
+);
