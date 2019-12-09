bf8e9f05c62dcf5deaa650ac7278a9a88cfe1b19
Florian Wessels <f.wessels@bitmotion.de>
2019-12-09 16:55:16 +0100

    [FEATURE] Introduce TYPO3 v10.2 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF['locate'] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],
