c87d97155697fe2c04844105a2935fb470e288b4
Florian Wessels <flo.wessels@gmail.com>
2020-12-30 17:52:15 +0100

    [TASK] Kickstart functional tests

---
+++ Tests/Functional/Store/Session/SessionStoreTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the "Auth0" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
+namespace Leuchtfeuer\Locate\Tests\Functional\Store\Session;
+
+use Leuchtfeuer\Locate\Store\SessionStore;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+/**
+ * @covers \Leuchtfeuer\Locate\Store\SessionStore
+ */
+class SessionStoreTest extends FunctionalTestCase
+{
+    /**
+     * @var SessionStore
+     */
+    protected $subject;
+
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/locate'
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new SessionStore();
+    }
+
+    /**
+     * @test
+     */
+    public function storeDataTest()
+    {
+        $this->subject->set('foo', 'bar');
+        self::assertSame('bar', $this->subject->get('foo'));
+    }
+}
