b2a21d0591133edf1339552e4be25a013440bd93
Florian Wessels <flo.wessels@gmail.com>
2020-12-30 17:53:47 +0100

    [BUGFIX] Update doc

--- Tests/Functional/Store/Session/SessionStoreTest.php
+++ Tests/Functional/Store/Session/SessionStoreTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the "Auth0" extension for TYPO3 CMS.
+ * This file is part of the "Locate" extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
