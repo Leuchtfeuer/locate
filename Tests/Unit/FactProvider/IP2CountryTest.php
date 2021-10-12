2fd2de70607e9e395ecac57941fd1b2405926933
Helmut Hummel <typo3@helhum.io>
2021-10-12 10:16:11 +0200

    Add unit tests to CI workflow and streamline CI and testing config

--- Tests/Unit/FactProvider/IP2CountryTest.php
+++ Tests/Unit/FactProvider/IP2CountryTest.php
@@ -1,5 +1,16 @@
 <?php
+
 declare(strict_types=1);
+
+/*
+ * This file is part of the "Locate" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ */
+
 namespace Leuchtfeuer\Locate\Tests\Unit\FactProvider;
 
 use Leuchtfeuer\Locate\FactProvider\IP2Country;
