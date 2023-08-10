08e2bc28a4ec84b9294f2950205705db500bb184
dkranz <d.kranz@leuchtfeuer.com>
2023-08-10 15:49:46 +0200

    [WIP] Update testing-framework and phpunit, update tests and GitHub CI workflow

--- Tests/Functional/FrontendTest.php
+++ Tests/Functional/FrontendTest.php
@@ -20,7 +20,7 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 class FrontendTest extends FunctionalTestCase
 {
-    protected $testExtensionsToLoad = [
+    protected array $testExtensionsToLoad = [
         'typo3conf/ext/locate'
     ];
 
