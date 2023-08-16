3864b1dc1fe366be1e63a38a066564d98d1d2f23
dkranz <d.kranz@leuchtfeuer.com>
2023-08-16 18:36:45 +0200

    [TASK] Update method to get typoscript settings

--- Classes/Middleware/LanguageRedirectMiddleware.php
+++ Classes/Middleware/LanguageRedirectMiddleware.php
@@ -19,10 +19,8 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use TYPO3\CMS\Core\Context\Context;
-use TYPO3\CMS\Core\Context\TypoScriptAspect;
-use TYPO3\CMS\Core\TypoScript\TemplateService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\BackendConfigurationManager;
 
 class LanguageRedirectMiddleware implements MiddlewareInterface
 {
@@ -55,16 +53,7 @@ class LanguageRedirectMiddleware implements MiddlewareInterface
 
     protected function getTypoScriptSetup(): array
     {
-        if (!$GLOBALS['TSFE']->tmpl instanceof TemplateService || empty($GLOBALS['TSFE']->tmpl->setup)) {
-            $context = GeneralUtility::makeInstance(Context::class);
-
-            if ($context->getPropertyFromAspect('typoscript', 'forcedTemplateParsing') === false) {
-                $context->setAspect('typoscript', new TypoScriptAspect(true));
-            }
-
-            $GLOBALS['TSFE']->getConfigArray();
-        }
-
-        return $GLOBALS['TSFE']->tmpl->setup;
+        $backendConfigurationManager = GeneralUtility::makeInstance( BackendConfigurationManager::class);
+        return $backendConfigurationManager->getTypoScriptSetup();
     }
 }
